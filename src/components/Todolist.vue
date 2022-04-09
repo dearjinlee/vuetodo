97e6a478090a6fdd8a057462bf3a8a3f085bb96c
dearjinlee <null>
2022-04-09 08:15:43 +0000

    Added todo-add/sub function

--- src/components/Todolist.vue
+++ src/components/Todolist.vue
@@ -7,10 +7,13 @@
             <h1>To - Do List</h1>
 
             <!-- ul.list_add -->
+			<!--
             <ul class="list_add">
                 <li class="list_add_tit"><p>등록 하기</p></li>
                 <li><p><input type="text"><button>등 록</button></p></li>
             </ul>
+			-->
+			<TodoInput @addTodo="addTodo" />
             <!--// ul.list_add -->
 
             <!-- ul.todo_list -->
@@ -19,10 +22,10 @@
                 <li class="todo_list_tit"><p>할 일</p></li>
 
                 <li class="del_btn" v-for="(item,index) in todolist" :key="index">
-                    <p><input type="checkbox" name="check1" value="">{{ item.todo }}</p>
+                    <p :class="{doneStyle:item.done}"><input type="checkbox" name="check1" v-model="item.done">{{ item.todo }}</p>
                     <ul class="todo_list_btn">
-                        <li><button>수 정</button></li>
-                        <li><button>삭 제</button></li>
+                        <li><button @click="updateTodo">수 정</button></li>
+                        <li><button @click="subTodo(index)">삭 제</button></li>
                     </ul>
                 </li>
 
@@ -36,18 +39,42 @@
 </template>
 
 <script>
+import TodoInput from './TodoInput.vue'
+
 export default {
   name: 'todolist',
+  components: {
+	  TodoInput,
+  },
   data: function() {
 	return {
 		todolist:[
-			{todo:"vue.js 공부하기"},
-			{todo:"독서 2p 이상"},
-			{todo:"보안 과제하기"},
-			{todo:"9시 스터디하기"},
-			{todo:"과제 채점"},
+			{done:false, todo:"vue.js 공부하기"},
+			{done:false, todo:"독서 2p 이상"},
+			{done:false, todo:"보안 과제하기"},
+			{done:false, todo:"9시 스터디하기"},
+			{done:true, todo:"과제 채점"},
 		]
 	}
   },
+  methods:{
+	  addTodo(val){
+		  console.log(val);
+		  this.todolist.push({done:false, todo:val});
+	  },
+	  subTodo(idx){
+		  this.todolist.splice(idx, 1);
+	  },
+	  updateTodo(idx){
+		  //this.todolist.splice(idx, 1);
+	  }
+  }
 }
 </script>
+
+<style>
+	.doneStyle {
+		text-decoration : line-through;
+		color: lightgray;
+	}
+</style>
