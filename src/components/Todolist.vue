f19dd4c5ecdd69144526b17e56a80afa7725d193
dearjinlee <null>
2022-04-09 08:45:39 +0000

    Added todo-completed-and-clean jobs

--- src/components/Todolist.vue
+++ src/components/Todolist.vue
@@ -19,8 +19,10 @@
             <!-- ul.todo_list -->
             <ul class="todo_list">
                 <li class="todo_list_tit"><p>할 일</p></li>
-                <li><p> {{ remaining }} / {{ todolist.length }} 건 처리 </p></li>
-
+                <li>
+					<p> {{ remaining }} / {{ todolist.length }} 건 처리 </p>
+                    <button @click="cleanTodo">처리완료삭제</button>
+				</li>
                 <li class="del_btn" v-for="(item,index) in todolist" :key="index">
                     <p :class="{doneStyle:item.done}"><input type="checkbox" name="check1" v-model="item.done">{{ item.todo }}</p>
                     <ul class="todo_list_btn">
@@ -77,6 +79,11 @@ export default {
 	  },
 	  subTodo(idx){
 		  this.todolist.splice(idx, 1);
+	  },
+	  cleanTodo(){
+		  this.todolist = this.todolist.filter(function(val){
+			  return val.done === false;
+		  	}).length;
 	  }
   }
 }
